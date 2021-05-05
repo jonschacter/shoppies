4dcb63d1b1313c29785331796a96f9545d9d90da
github username <schacter.jon@gmail.com>
2021-05-04 19:30:38 -0700

    create MovieList component

---
+++ src/MovieList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+    render() {
+        return (
+            <div className="movie-list">
+                HELLO
+            </div>
+        );
+    }
+}
+
+export default MovieList;
