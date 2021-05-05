c8b16619b3912c756bddd2dd149758ffcd18aa00
github username <schacter.jon@gmail.com>
2021-05-05 14:03:26 -0700

    placeholder state to determine if button should be disabled

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
-class MovieCard extends Component {
+class MovieCard extends Component {  
+    constructor(props){
+        super(props)
+
+        this.state = {
+            button: true
+        }
+    }
+    
     render() {
+        const { Title, Year } = this.props.movie
         return (
             <div className="movie-card">
-               { this.props.movie.Title } 
+               { Title } ({ Year })
+               <button className="nominate-button" disabled={!this.state.button}>Nominate</button>
             </div>
         );
     }
