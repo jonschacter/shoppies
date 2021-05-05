674c6fd94d6a4583389af28fd81015298fffff1a
github username <schacter.jon@gmail.com>
2021-05-04 18:46:48 -0700

    change App component to Shoppies

---
+++ src/Shoppies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Shoppies extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>The Shoppies</h1>
+            </div>
+        );
+    }
+}
+
+export default Shoppies;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Shoppies from './Shoppies';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Shoppies />
   </React.StrictMode>,
   document.getElementById('root')
 );
