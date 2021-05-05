83f340358a43b63e44a7b71c0ba0f0da69851dd9
github username <schacter.jon@gmail.com>
2021-05-04 19:09:16 -0700

    add controlled form for Title query

--- src/Shoppies.js
+++ src/Shoppies.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class Shoppies extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: "",
+            movies: []
+        }
+    }
+    
+    handleChange = (e) => {
+        this.setState({
+            query: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>The Shoppies</h1>
+                <div className="movie-search-form">
+                    <form>
+                        <label>Movie Title:</label>
+                        <input type="text" onChange={this.handleChange} name="search-title" value={this.state.query} />
+                    </form>
+                </div>
             </div>
         );
     }
