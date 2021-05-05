d944e3a8148f7a68fb673fe545b55a6b3e7e87e4
github username <schacter.jon@gmail.com>
2021-05-05 13:45:49 -0700

    add className for css

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MovieCard extends Component {
     render() {
         return (
-            <div>
+            <div className="movie-card">
                { this.props.movie.Title } 
             </div>
         );
