0f8f77673c371ef59a99536848bb993524d7e83e
github username <schacter.jon@gmail.com>
2021-05-05 14:14:21 -0700

    pass addNominate to MovieCards

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import MovieCard from './MovieCard.js'
 
-function MovieList({ movies }) {
+function MovieList({ movies, addNominate }) {
     return (
         <div className="movie-list">
             { movies.map(movie => {
-                return < MovieCard key={movie.imdbID} movie={movie}/>
+                return < MovieCard key={movie.imdbID} movie={movie} addNominate={addNominate} />
             }) }
         </div>
     );
