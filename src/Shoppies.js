9ea27110cd0e32e56b4472ed183592b090bc95bc
github username <schacter.jon@gmail.com>
2021-05-05 15:22:55 -0700

    fetch movies again if form is submitted

--- src/Shoppies.js
+++ src/Shoppies.js
@@ -14,6 +14,8 @@ class Shoppies extends Component {
     
     handleSubmit = (e) => {
         e.preventDefault()
+        
+        this.getMovies(this.state.query)
     }
 
     handleChange = (e) => {
