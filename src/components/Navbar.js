61f62f5aed82de5155b0ec3dc8914901d123eced
github username <schacter.jon@gmail.com>
2021-05-07 14:31:11 -0700

    add navbar links for Home, Log In, and Sign Up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ loggedIn }) => {
     return (
         <div className="navbar">
-            NAVBAR
+            <Link to="/">Home</Link>
+            { loggedIn ? <p>In</p> : <> | <Link to="/login">Log In</Link> | <Link to="/signup">Sign Up</Link></> }
         </div>
     );
 };
 
-export default Navbar;
+const mapStateToProps = ({ user }) => {
+    return {
+        loggedIn: !!user
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);
