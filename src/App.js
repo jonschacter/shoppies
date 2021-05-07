e480b1cef411c5decc42affdcb788f0c9bf6cfad
github username <schacter.jon@gmail.com>
2021-05-07 14:31:42 -0700

    create UserForm component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/Navbar.js'
 import Shoppies from './Shoppies.js'
+import UserForm from './components/user/UserForm.js'
 
 class App extends Component {
     render(){
@@ -14,6 +15,8 @@ class App extends Component {
                     <h1>The Shoppies</h1>
                     <Switch>
                         <Route exact path="/" component={Shoppies} />
+                        <Route exact path="/signup" render={(props) => <UserForm {...props} type="Sign Up" />} />
+                        <Route exact path="/login" render={(props) => <UserForm {...props} type="Log In" />} />
                     </Switch>
                 </Router>
             </div>
---
+++ src/components/user/UserForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+    render() {
+        return (
+            <div className="user-form">
+                Form
+            </div>
+        );
+    }
+}
+
+export default UserForm;
