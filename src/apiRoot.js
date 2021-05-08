9a1e62e1bb87fd86b884b94785ec360be3cff610
github username <schacter.jon@gmail.com>
2021-05-07 19:54:25 -0700

    use https for omdb api

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,9 +1,9 @@
 // OMDB base url
-export const OMDB_API_ROOT = `http://omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`
+export const OMDB_API_ROOT = `https://omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`
 
 // flexible backend base url to allow for development and production
 const DEV_URL = 'http://localhost:3001';
-// !!!ADD ME WHEN READY FOR PRODUCTION!!!
+
 const PROD_URL = 'https://jschacter-shoppies-api.herokuapp.com';
 
 export const BACKEND_API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
