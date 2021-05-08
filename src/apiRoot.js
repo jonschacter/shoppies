04fe06afcd6df1580789505e3cd90ae0b1d587a7
github username <schacter.jon@gmail.com>
2021-05-07 17:57:05 -0700

    add heroku api url

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -4,6 +4,6 @@ export const OMDB_API_ROOT = `http://omdbapi.com/?apikey=${process.env.REACT_APP
 // flexible backend base url to allow for development and production
 const DEV_URL = 'http://localhost:3001';
 // !!!ADD ME WHEN READY FOR PRODUCTION!!!
-const PROD_URL = null;
+const PROD_URL = 'https://jschacter-shoppies-api.herokuapp.com/';
 
 export const BACKEND_API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
